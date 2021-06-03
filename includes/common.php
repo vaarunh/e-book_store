a80818644ed93ab503de356a44312694a6f853e7
Varun's Notebook <varunherlekar22@gmail.com>
2021-06-03 21:38:17 +0530

    Heroku Setup Completed

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("remotemysql.com", "0wOCemB7jz", "KHWw6QdFus", "0wOCemB7jz")
+    $con = mysqli_connect("localhost", "root", "", "ecommerce")
     or die(mysqli_error($con));
     if(!isset($_SESSION)){
       session_start();
